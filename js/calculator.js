42f6b7cbfb8521621dbf7d1d6b675148398cc739
cоня <sofiasioutch@yandex.ru>
2026-08-17 21:41:04 +0300

    Add files via upload

--- js/calculator.js
+++ js/calculator.js
@@ -10,27 +10,28 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("numeral1"),
     document.getElementById("numeral2"),
   ];
-  const result = document.getElementById("result");
+  const resultInp = document.getElementById("result");
+  const buttonsMathOperations = [
+    additionBtn,
+    subtractionBtn,
+    multiplicationBtn,
+    divisionBtn,
+    exponentiationBtn,
+    remainderBtn,
+  ];
+
+  // let numeral1 = document.getElementById("numeral1".value);
+  // let numeral2 = document.getElementById("numeral2".value);
 
   console.log(
     "ИНСТРУКЦИЯ КАЛЬКУЛЯТОР    " +
       "1. введите значения в левое поле и правое поле  " +
-      "2. нажмите кнопку с необходимой операцией  " +
-      "3. нажмите на нижнее поле  ",
+      "2. нажмите кнопку с необходимой операцией  ",
   );
 
-  let numeral1 = document.getElementById("numeral1");
-  let numeral2 = document.getElementById("numeral2");
-
-  /* buttonsMathOperations.forEach((button) =>
-    addEventListener("click", () => variableOutput(numeral1, numeral2)),
-  );*/
-
-  // inputs.forEach((input) =>
-  //   addEventListener("click", () => {
-  //     variableOutput(numeral1, numeral2);
-  //   }),
-  // );
+  buttonsMathOperations.forEach((elem) =>
+    elem.addEventListener("click", () => assignment()),
+  );
 
   OnChangeInput(
     inputs,
@@ -41,4 +42,10 @@ document.addEventListener("DOMContentLoaded", function () {
     exponentiationBtn,
     remainderBtn,
   );
+  additionBtn.addEventListener("click", () => addition());
+  subtractionBtn.addEventListener("click", () => subtraction());
+  multiplicationBtn.addEventListener("click", () => multiplication());
+  divisionBtn.addEventListener("click", () => division());
+  exponentiationBtn.addEventListener("click", () => exponentiation());
+  remainderBtn.addEventListener("click", () => remainder());
 });
